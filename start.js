ee7ab03c060eaeca9eea2aed88e6ce3eda8e15fa
ferhter <126424068+ferhter@users.noreply.github.com>
2023-03-30 12:38:39 -0600

    Create start.js

---
+++ start.js
@@ -0,0 +1,19 @@
+Wi-Fi:
+Dirección IP del nodo: [0.0.0.0] Id. de ámbito : []
+
+  
+
+Ethernet:
+Dirección IP del nodo: [192.168.1.69] Id. de ámbito : []
+
+  
+
+Conexión de red Bluetooth:
+Dirección IP del nodo: [0.0.0.0] Id. de ámbito : []
+
+   
+
+Wi-Fi:
+Dirección IP del nodo: [0.0.0.0] Id. de ámbito : []
+
+    
