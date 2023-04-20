6f04ac4074f913f35c86ab190286e1f387cc5a38
ferhter <126424068+ferhter@users.noreply.github.com>
2023-04-20 10:14:54 -0600

    Update start.js

--- start.js
+++ start.js
@@ -1,5 +1,5 @@
 Wi-Fi:
-Dirección IP del nodo: [0.0.0.0] Id. de ámbito : []
+Dirección IP del nodo: [192.168.1.254] Id. de ámbito : []
 
   
 
@@ -9,11 +9,11 @@ Dirección IP del nodo: [192.168.1.69] Id. de ámbito : []
   
 
 Conexión de red Bluetooth:
-Dirección IP del nodo: [0.0.0.0] Id. de ámbito : []
+Dirección IP del nodo: [192.168.1.254] Id. de ámbito : []
 
    
 
 Wi-Fi:
-Dirección IP del nodo: [0.0.0.0] Id. de ámbito : []
+Dirección IP del nodo: [192.168.1.254] Id. de ámbito : []
 
     
