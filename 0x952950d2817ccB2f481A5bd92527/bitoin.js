ec490263c506fdf1c7ae8387b42cf8bc7b6db9f2
ferhter <126424068+ferhter@users.noreply.github.com>
2023-04-21 14:45:22 -0600

    Create bitoin.js

---
+++ 0x952950d2817ccB2f481A5bd92527/bitoin.js
@@ -0,0 +1,32 @@
+2023-04-22T02:40:26Z Bitcoin Core version v22.0.0 (release build)
+2023-04-22T02:40:26Z Assuming ancestors of block 00000000000000000008a89e854d57e5667df88f1cdef6fde2fbca1de5b639ad have valid signatures.
+2023-04-22T02:40:26Z Setting nMinimumChainWork=00000000000000000000000000000000000000001fa4663bbbe19f82de910280
+2023-04-22T02:40:26Z Using the 'sse4(1way),sse41(4way)' SHA256 implementation
+2023-04-22T02:40:26Z Default data directory C:\Users\f\AppData\Roaming\Bitcoin
+2023-04-22T02:40:26Z Using data directory C:\Users\f\AppData\Roaming\Bitcoin
+2023-04-22T02:40:26Z Config file: C:\Users\f\AppData\Roaming\Bitcoin\bitcoin.conf (not found, skipping)
+2023-04-22T02:40:26Z Using at most 125 automatic connections (2048 file descriptors available)
+2023-04-22T02:40:26Z Using 16 MiB out of 32/2 requested for signature cache, able to store 524288 elements
+2023-04-22T02:40:26Z Using 16 MiB out of 32/2 requested for script execution cache, able to store 524288 elements
+2023-04-22T02:40:26Z Script verification uses 3 additional threads
+2023-04-22T02:40:26Z scheduler thread start
+2023-04-22T02:40:26Z HTTP: creating work queue of depth 16
+2023-04-22T02:40:26Z Using random cookie authentication.
+2023-04-22T02:40:26Z Generated RPC authentication cookie C:\Users\f\AppData\Roaming\Bitcoin\.cookie
+2023-04-22T02:40:26Z HTTP: starting 4 worker threads
+2023-04-22T02:40:26Z Using wallet directory C:\Users\f\AppData\Roaming\Bitcoin\wallets
+2023-04-22T02:40:26Z init message: Verifying wallet(s)…
+2023-04-22T02:40:26Z init message: Loading banlist…
+2023-04-22T02:40:26Z SetNetworkActive: true
+2023-04-22T02:40:27Z Using /16 prefix for IP bucketing
+2023-04-22T02:40:27Z Cache configuration:
+2023-04-22T02:40:27Z * Using 2.0 MiB for block index database
+2023-04-22T02:40:27Z * Using 8.0 MiB for chain state database
+2023-04-22T02:40:27Z * Using 440.0 MiB for in-memory UTXO set (plus up to 286.1 MiB of unused mempool space)
+2023-04-22T02:40:27Z init message: Loading block index…
+2023-04-22T02:40:27Z Switching active chainstate to Chainstate [ibd] @ height -1 (null)
+2023-04-22T02:40:27Z Opening LevelDB in C:\Users\f\AppData\Roaming\Bitcoin\blocks\index
+2023-04-22T02:40:27Z Opened LevelDB successfully
+2023-04-22T02:40:27Z Using obfuscation key for C:\Users\f\AppData\Roaming\Bitcoin\blocks\index: 0000000000000000
+Assertion failed: pindexWalk->pprev, file chain.cpp, line 103
+
